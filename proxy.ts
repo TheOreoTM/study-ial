c8d808489a597bf23b18711e7da7d5c9ae097c96
TheOreoTM <qwertzybad@gmail.com>
2025-11-29 23:13:39 +0500

    refactor: rename middleware.ts to proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -2,7 +2,7 @@ import { stackServerApp } from "@/stack/server";
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
     const user = await stackServerApp.getUser();
     const path = request.nextUrl.pathname;
 
